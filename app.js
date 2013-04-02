9144a9cc2828dc0aeeb0116a1b6f2ced06fabd83
Stephan Smith <stephan.smith.bc93@gmail.com>
2013-04-02 11:31:17 -0400

    Moved the web.js to db.js and app.js

---
+++ app.js
@@ -0,0 +1,115 @@
+var express    = require('express'),
+    url        = require('url'),
+    swagger    = require('swagger-node-express'),
+    argv       = require('optimist').argv;
+
+AppManager = (function() {
+
+  var port = argv.port || process.env.PORT || 4000;
+
+  var configureApp = function(app, db) {
+
+    app.configure(function() {
+
+      app.use(express.bodyParser());
+
+      app.set('port', process.env.PORT || 4000);
+      app.set('views', __dirname + '/swagger-ui-1.1.7');
+      app.set('view options', { layout: false, pretty: false });
+      app.set('view engine', 'jade');
+
+      app.use(express.logger('dev'));
+      swagger.setAppHandler(app);
+
+      app.use(function(req, res, next) {
+        var url = require('url');
+        var queryURL = url.parse(req.url, true);
+        req.urlparams = queryURL.query;
+        next();
+      });
+
+      swagger.addValidator(
+        function validate(req, path, httpMethod) {
+
+          if ("POST" == httpMethod || "DELETE" == httpMethod || "PUT" == httpMethod) {
+            var apiKey = req.headers["api_key"];
+            if (!apiKey) {
+              apiKey = url.parse(req.url,true).query["api_key"]; }
+            if ("special-key" == apiKey) {
+              return true; 
+            }
+            return false;
+          }
+
+          return true;
+        }
+      );
+
+    });
+  };
+
+  var configureAPI = function(app) {
+
+    var resources = require("./resources/index");
+
+    // Add models and methods to swagger
+    swagger.addModels(resources.content.models)
+      // Content CRUD
+      .addGet(     resources.content.get      )
+      .addGet(     resources.content.byTags   )
+      .addGet(     resources.content.byStatus )
+      .addPost(    resources.content.add      )
+      .addPut(     resources.content.update   )
+      .addDelete(  resources.content.delete   )
+
+      // Model CRUD
+      .addGet(     resources.models.get       )
+      .addPost(    resources.models.add       )
+      .addPut(     resources.models.update    )
+      .addDelete(  resources.models.delete    )
+    ;
+
+    // Configures the API's base path and api version.
+    swagger.configure(process.env.API_URL || 'http://localhost:4000', '0.1');
+
+    // Serve up swagger ui at /docs via static route
+    var docs_handler = express.static(__dirname + '/swagger-ui-1.1.7/');
+
+    // Direct all root traffic to the UI.
+    app.get('/', function(req, res) {
+      res.redirect('/docs');
+    });
+
+    app.get('/docs/', function(req, res) {
+      res.render('index', { API_URL: process.env.API_URL || 'http://localhost:4000'});
+    });
+
+    app.get(/^\/docs(\/.*)?$/, function(req, res, next) {
+      if (req.url === '/docs') { // express static barfs on root url w/o trailing slash
+        res.writeHead(302, { 'Location' : req.url + '/' });
+        res.end();
+        return;
+      }
+      // take off leading /docs so that connect locates file correctly
+      req.url = req.url.substr('/docs'.length);
+      return docs_handler(req, res, next);
+    });
+
+  };
+
+  return {
+    start: function(db) {
+      var app = module.exports = express();
+
+      configureApp(app, db);
+      configureAPI(app);
+
+      app.listen(port);
+
+      console.log("Staring Express Server on port %d in %s mode", port);
+      return app;
+    }
+  };
+})();
+
+exports.AppManager = AppManager;
---
+++ db.js
@@ -0,0 +1,33 @@
+var mongodb    = require('mongodb');
+
+DbManager = (function() {
+
+  var enabled_auth = true;
+
+  var mongoCon = require('url').parse( process.env.MONGOHQ_URL );
+
+  mongoCon.auth_ex = mongoCon.auth.split(':');
+  var _dbUser = mongoCon.auth_ex[0];
+  var _dbPass = mongoCon.auth_ex[1];
+  var _dbname = mongoCon.path.replace('/', '');
+
+  var _server = new mongodb.Server(mongoCon.hostname, parseInt(mongoCon.port), { safe: false });
+  var db     = new mongodb.Db(_dbname, _server, { native_parser: true });
+
+  return {
+    getDb: function() {
+      return db;
+    },
+    authenticate: function(client, cb) {
+      if (enabled_auth) {
+        client.authenticate( _dbUser, _dbPass, function(err, success) { 
+          cb(client)
+        });
+      } else {
+        cb(client);
+      }
+    }
+  }
+})();
+
+exports.DbManager = DbManager;
